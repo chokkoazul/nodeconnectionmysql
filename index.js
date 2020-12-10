6b684135b6af33f8a5f7ef5542f30965472bb066
Cristian Osorio <cristian.osorio@globant.com>
2020-12-10 15:44:05 -0300

    se modifica la base de datos techtalks

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'clave',
-    database: 'universidades'
+    database: 'techtalks'
 });
 
 connection.connect((err) => {
